cd84c843a2c9a6e8541a7c92376a753157ac9857
va18aam <va18aam@ENT-D410-49.herts.ac.uk>
2019-04-01 14:29:04 +0100

    All test Cases

--- test/es/uvigo/esei/dojos/swing/todo/core/TodoListTest.java
+++ test/es/uvigo/esei/dojos/swing/todo/core/TodoListTest.java
@@ -41,7 +41,103 @@ public class TodoListTest {
     /**
      * Test of moveUp method, of class TodoList.
      */
-   public void testSize() 
+   public void testMoveUp() 
+{
+    System.out.println("moveup");
+    int i = 0;
+    TodoList instance = new TodoList();
+    String name = "vothaiho";
+    instance.add(name);
+    name = "ravi";
+    instance.add(name);
+    instance.moveUp(1);
+    String expResult = "ravi";
+    String result =  instance.elementAt(0);
+    assertEquals(expResult, result);
+   System.out.println("entry is moved up");
+}
+
+    
+           
+        
+    
+    
+
+@Test
+public void testMoveDown() 
+{
+
+
+    System.out.println("movedown");
+    int i = 0;
+    TodoList instance = new TodoList();
+    String name = "vothaiho";
+    instance.add(name);
+    name = "ravi";
+    instance.add(name);
+    instance.moveDown(0);
+    String expResult = "vothaiho";
+    String result =  instance.elementAt(1);
+    assertEquals(expResult, result);
+   System.out.println("entry is moved down");
+}
+
+
+
+@Test
+public void testAdd()
+{
+    System.out.println("Add");
+    TodoList instance = new TodoList();
+     String name = "vothaiho";
+    instance.add(name);
+    int i = instance.size();
+    assertTrue(i > 0);
+    System.out.println("entry is added");
+}
+
+@Test
+public void testEditAt() 
+        
+{
+    System.out.println("EditAt");
+    TodoList instance = new TodoList();
+    int i = 0;
+    String Name = "Ravi";
+    instance.add(Name);
+    Name = "Voohitha";
+    instance.add(Name);
+    Name = "sai";
+    instance.add(Name);
+    String result = instance.elementAt(1);
+    String newValue = "suresh";
+    instance.editAt(1, newValue);
+    result = instance.elementAt(1);
+    System.out.println("Editing at entry ");
+    assertEquals(newValue, result);
+    System.out.println("entry is edited");
+    
+    
+
+}
+
+@Test
+public void testRemoveAt() 
+{
+    System.out.println("removeAt");
+    int i = 0;
+    TodoList instance = new TodoList();
+    String name = "voohitha";
+    instance.add(name);
+    int size1 = instance.size();
+    instance.removeAt(0);
+    int size2 = instance.size();
+    assertTrue(size2<size1);
+    System.out.println("entry is removed");
+}
+
+@Test
+public void testSize() 
 {
     System.out.println("size");
     TodoList instance = new TodoList();
@@ -54,6 +150,5 @@ public class TodoListTest {
     assertEquals(expResult, result);
     System.out.println("list size is"+result);
 }
-           
-
+    
 }

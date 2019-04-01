518a0fc09ee92878da8d62e70439facf3c26e94e
va18aam <va18aam@ENT-D410-49.herts.ac.uk>
2019-04-01 13:19:26 +0100

    mock practical 123

---
+++ test/es/uvigo/esei/dojos/swing/todo/core/TodoListTest.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.uvigo.esei.dojos.swing.todo.core;
+
+import java.util.Iterator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author comqsjb
+ */
+public class TodoListTest {
+    
+    public TodoListTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of moveUp method, of class TodoList.
+     */
+    @Test
+public void testMoveUp() 
+{
+fail("Not yet implemented");
+}
+
+@Test
+public void testMoveDown() 
+{
+fail("Not yet implemented");
+}
+
+@Test
+public void testAdd() 
+{
+fail("Not yet implemented");
+}
+
+@Test
+public void testEditAt() 
+{
+fail("Not yet implemented");
+}
+
+@Test
+public void testRemoveAt() 
+{
+fail("Not yet implemented");
+}
+
+@Test
+public void testSize() 
+{
+fail("Not yet implemented");
+}
+    
+}
